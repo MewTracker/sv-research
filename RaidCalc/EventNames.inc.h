3452bd550ca5a4d6360fd248701b59d62ab411f5
MewTracker <96765737+MewTracker@users.noreply.github.com>
2025-12-31 22:54:06 +0100

    Added Hydreigon event

--- RaidCalc/EventNames.inc.h
+++ RaidCalc/EventNames.inc.h
@@ -84,4 +84,5 @@ static const char *event_names[] =
     "Shiny Ting-Lu Spotlight",
     "Shiny Chi-Yu Spotlight",
     "Iron Valiant the Unrivaled and Roaring Moon the Unrivaled",
+    "Hydreigon the Unrivaled",
 };
